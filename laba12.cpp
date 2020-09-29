f019704e12e659b2dbf21f7c2d7ec3daa1227ae7
Vanya215 <32173435+Vanya215@users.noreply.github.com>
2020-09-29 22:54:10 +0300

    Add files via upload

---
+++ laba12.cpp
@@ -0,0 +1,26 @@
+﻿// laba12.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+#include <stdio.h>
+
+int main()
+{
+	std::string s;
+	int n = 0;
+	bool t = false;
+	getline(std::cin, s);
+	for (int i = 0; i < s.length(); i++) {
+		if (s[i] == ' ' && t) {
+			n++;
+			t = false;
+		}
+		if (s[i] != ' ' && !t)
+			t = true;
+		
+		if (i == s.length() - 1 && s[i] != ' ')
+			n++;		
+	}
+	printf("%d\n", n);
+}
